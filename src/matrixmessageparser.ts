507a6e4a7e06df9758b640f6c7a3eb0a38f67fda
Sorunome <mail@sorunome.de>
2020-01-19 20:07:02 +0100

    remove unneeded console.log

--- src/matrixmessageparser.ts
+++ src/matrixmessageparser.ts
@@ -121,7 +121,6 @@ export class MatrixMessageParser {
         if (language) {
             text = language[1] + text;
         } else if (opts.determineCodeLanguage) {
-            console.log(highlightjs.highlightAuto(text).language);
             text = highlightjs.highlightAuto(text).language + text;
         }
         return text;
